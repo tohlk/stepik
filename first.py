9cf12ae548cd1e0411ed0434924b2720fa5a8233
Zham_Raf <raf280400@mail.ru>
2025-09-08 19:40:33 +0300

    StepikDay1

---
+++ first.py
@@ -0,0 +1,10 @@
+def get_sum(a, b) -> int:
+    return a + b
+
+
+print(get_sum(5, 7))
+
+
+
+print("A great man doesn't seek to lead.", end='\n')
+print("He's called to it. And he answers.", end='\n')
